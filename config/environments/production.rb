9224cf99cd65cdc517c6d940394c73cb77b4d005
Lucas Manzano Farias <lucasmfarias1@gmail.com>
2018-10-01 20:47:28 -0300

    fix production host for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.default_url_options = { host: 'frozen-citadel-60411.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'lolsolokill.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
