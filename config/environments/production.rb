867963f7ea76693532dfe103d6c725383c0f159e
Lucas Manzano Farias <lucasmfarias1@gmail.com>
2018-10-01 19:02:40 -0300

    1st attempt email heroku config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,18 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.zoho.com",
+    :port                 => 465,
+    :user_name            => 'solokill@zoho.com',
+    :password             => ENV["ZOHO_PW"],
+    :authentication       => :login,
+    :ssl                  => true,
+    :tls                  => true,
+    :enable_starttls_auto => true
+  }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
