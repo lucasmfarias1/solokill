3bc8764833076818eb41ee1b2c7a627486a73152
Lucas Manzano Farias <lucasmfarias1@gmail.com>
2018-10-24 19:37:33 -0300

    make it so 'desvincular' only shows when you actually have a lol acc

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :store_user_location!, if: :storable_location?
 
   private
-  
+
     def storable_location?
       request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
     end
