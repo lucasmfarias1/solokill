9cf77b0b7a87df213967823b4ee3ad867b7faaa5
Lucas Manzano Farias <lucasmfarias1@gmail.com>
2018-10-27 18:01:15 -0300

    redesign logout/login/settings links, implement infinite scrolling 100px away from bottom of page; redimension login/signup pages; add favicon

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,7 +22,14 @@ $(document).on('turbolinks:load', function() {
   $(function () {
     $('[data-toggle="tooltip"]').tooltip()
   });
-  $("#new-post-alert").click(function() {
-    $(document).attr("title", "Solokill");
+
+  $(window).scroll(function() {
+    if($(window).scrollTop() + $(window).height() >
+       $(document).height() - 100) {
+      var more_posts_url = $("#load_more_link").attr('href');
+        if (more_posts_url) {
+          $.getScript(more_posts_url);
+        }
+    }
   });
 });
